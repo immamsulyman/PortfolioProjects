03efc87dfffcce78bfede9be8e1c2628ac0c9782
immamsulyman <39934448+immamsulyman@users.noreply.github.com>
2022-03-10 14:26:46 +0100

    Update Portfolio Project data exploration.sql

--- Portfolio Project data exploration.sql
+++ Portfolio Project data exploration.sql
@@ -1,3 +1,4 @@
+  --creating View to store data for later visualizations
  CREATE VIEW PercentPopulationVaccinated as
  SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
  SUM(CONVERT(bigint, vac.new_vaccinations)) OVER(PARTITION BY dea.Location Order by dea.date) AS rollingPeopleVaccinated
