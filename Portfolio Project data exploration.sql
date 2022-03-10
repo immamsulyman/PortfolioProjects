353c387285be10f68360dbc0f204816b47a4c0fb
immamsulyman <39934448+immamsulyman@users.noreply.github.com>
2022-03-10 14:22:35 +0100

    Add files via upload

---
+++ Portfolio Project data exploration.sql
@@ -0,0 +1,11 @@
+ CREATE VIEW PercentPopulationVaccinated as
+ SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+ SUM(CONVERT(bigint, vac.new_vaccinations)) OVER(PARTITION BY dea.Location Order by dea.date) AS rollingPeopleVaccinated
+FROM PortfolioProject..CovidDeaths dea
+JOIN PortfolioProject..CovidVaccinations vac
+ ON dea.location = vac.location
+ AND dea.date = vac.date
+ where dea.continent is not null
+
+ Select *
+ From PercentPopulationVaccinated
